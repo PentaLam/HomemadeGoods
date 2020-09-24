204194257497ab7bb48a05eaf392d7d992f7239c
hmontesdeoca <montes.de.oca.howard@gmail.com>
2020-09-24 00:55:22 -0700

    added chai dependencies, and made a test file using chai

---
+++ test/chai_test.js
@@ -0,0 +1,12 @@
+//used an example to demonstarte testing with chai - Howard Montes De Oca
+"use strict";
+
+var expect = require('chai').expect;
+
+describe('Math', function() {
+	    describe('#abs()', function() {
+		            it('should return positive value of given negative number', function() {
+				                expect(Math.abs(-5)).to.be.equal(5);
+				            });
+		        });
+});
