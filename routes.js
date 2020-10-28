5258643af750011a40e9d33a8b35760b37de9b7a
Juan Vera <jlverarosas@cpp.edu>
2020-10-27 22:08:29 -0700

    Added route to push mongoose data

--- routes.js
+++ routes.js
@@ -5,4 +5,17 @@ const router = require('express').Router();
 
 router.get('/listing', listingController.getListings);
 
+router.route("/insertdata").post(function(req, res) {
+  kennel.insert(
+      {},
+    function(err, result) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
 module.exports = router;
