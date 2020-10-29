fd0c3ead8e79d8f52335e528dff48b889241b2f6
Arturo Blandon <arturob1024@gmail.com>
2020-10-29 12:08:59 -0700

    Basic react template

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import {Container} from 'react-bootstrap';
+import ListingCard from '../components/ListingCard';
+
+const Home = () => {
+    return (  
+        <div>
+            <Container>
+                <ListingCard/>
+            </Container>
+        </div>
+    );
+}
+ 
+export default Home;
