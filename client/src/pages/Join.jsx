42900b4801b3abd6e7d7acbdc27120d41d1b71b8
hmontesdeoca <montes.de.oca.howard@gmail.com>
2020-11-01 10:39:39 -0800

    added the forms as a compnent to the page

--- client/src/pages/Join.jsx
+++ client/src/pages/Join.jsx
@@ -1,64 +1,10 @@
-import { Container, Row, Form } from 'react-bootstrap'
+import { Container} from 'react-bootstrap'
+import FormPage from '../components/FormPage';
 
 const Join  = () => {
     return (
 <Container>
-  <Form>
-      <Form.Group className="w-50">
-            <Form.Label>Business Name</Form.Label>
-            <Form.Control/>
-        </Form.Group>
-        <Form.Group className="w-50">
-            <Form.Label>Business Type</Form.Label>
-
-            <Form.Check
-            type="radio"
-            label="food"
-            name="businessType"
-            id="food"/>
-
-            <Form.Check
-            type="radio"
-            label="services"
-            name="businessType"
-            id="services"/>
-
-            <Form.Check
-            type="radio"
-            label="craft"
-            name="businessType"
-            id="craft"/>
-
-            <Form.Check
-            type="radio"
-            label="art"
-            name="businessType"
-            id="art"/>
-
-            <Form.Check
-            type="radio"
-            label="other"
-            name="businessType"
-            id="other"/>
-      </Form.Group>
-      <Form.Group className="w-50">
-            <Form.Label>Contact Information</Form.Label>
-            <Form.Control
-            placeholder="Phone Number, Email, etc..."/>
-        </Form.Group>
-      <Form.Group className="w-50">
-            <Form.Label>Location</Form.Label>
-            <Form.Control
-            placeholder="zip code"/>
-      </Form.Group>
-      <Form.Group className="w-50">
-          <Form.Label>Biography</Form.Label>
-          <Form.Control
-          placeholder="Tell us about yourself!"
-          as="textarea"
-          rows={3} />
-      </Form.Group>
-  </Form>
+      <FormPage/>
 </Container>
     );
 }
