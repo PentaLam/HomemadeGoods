e5e77bd95feeb6810d0f08809eaabc496d7d94b1
David Lao <lao38452@gmail.com>
2020-11-12 10:13:39 -0800

    added ant design library

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import {Container, Jumbotron, Button} from 'react-bootstrap';
+import { Layout } from 'antd';
 //import ListingCard from '../components/ListingCard';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Header } from 'antd/lib/layout/layout';
 
 const pStyle = {
     fontSize: '15px',
@@ -9,20 +11,25 @@ const pStyle = {
   };
 
 const Home = () => {
-    return (  
-        <div>
-            <Container>
-                <Jumbotron>
-                    <h1>HomeMade Goods</h1>
-                    <p style={pStyle}>
-                        Where the shit is good and the goods are better than shit.
-                    </p>
-                    <p>
-                        <Button variant="primary">Learn more</Button>
-                    </p>
-                </Jumbotron>
-            </Container>
-        </div>
+    return (
+        <Layout>
+            <Header>
+                
+            </Header>
+        </Layout> 
+        // <div>
+        //     <Container>
+        //         <Jumbotron>
+        //             <h1>HomeMade Goods</h1>
+        //             <p style={pStyle}>
+        //                 Where the shit is good and the goods are better than shit.
+        //             </p>
+        //             <p>
+        //                 <Button variant="primary">Learn more</Button>
+        //             </p>
+        //         </Jumbotron>
+        //     </Container>
+        // </div>
     );
 }
  
