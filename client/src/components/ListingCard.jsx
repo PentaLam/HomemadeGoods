3597f81ea0c2e44f07e5d74e1a9e6f75b4946b45
Arturo Blandon <arturob1024@gmail.com>
2020-11-16 17:31:45 -0800

    Added business type images to cards

--- client/src/components/ListingCard.jsx
+++ client/src/components/ListingCard.jsx
@@ -1,25 +1,45 @@
-import {Card} from 'react-bootstrap';
+import {Card, Button} from 'react-bootstrap';
 
 const ListingCard = (props) => {
 
     let shortDescription = props.listing.description.substring(0,30)
+    let image;
+    switch (props.listing.businessType) {
+        case "food":
+            image = "../images/spaguetti.png"
+            break;
+        case "services":
+            image = "../images/customer-support.png"
+            break;
+        case "craft":
+            image = "../images/tool-box.png"
+            break;
+        case "art":
+            image = "../images/palette.png"
+            break;
+        default:
+            image = "../images/idea.png"
+            break;
+    }
 
     if(props.listing.description.length > 30){
         shortDescription += "...";
     }
 
     return (  
-        <div>
-            <Card style={{ width: '18rem' }}>
+        <div className="mb-3">
+            <Card className="d-flex flex-row">
                 <Card.Body>
                     <Card.Title> {props.listing.businessName} </Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">{props.listing.businessType}</Card.Subtitle>
                     <Card.Text>
                         {shortDescription}
                     </Card.Text>
-                    <Card.Link href={`/listing/${props.listing._id}`}>View</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
+                    <Card.Link href={`/listing/${props.listing._id}`}>View more</Card.Link>
                 </Card.Body>
+                <div style={{margin: "auto"}} className="mr-2">
+                    <img src={image} className="w-25 h-25 float-right"></img>
+                </div>
             </Card>
         </div>
     );
